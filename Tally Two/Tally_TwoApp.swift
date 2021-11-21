a644e17a3db2173b5ed3138a794540151f8db3c0
LuckDuracell <lukedru@yahoo.com>
2021-11-20 23:52:35 -0800

    Initial Commit

---
+++ Tally Two/Tally_TwoApp.swift
@@ -0,0 +1,17 @@
+//
+//  Tally_TwoApp.swift
+//  Tally Two
+//
+//  Created by Luke Drushell on 11/20/21.
+//
+
+import SwiftUI
+
+@main
+struct Tally_TwoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
